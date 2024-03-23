8f6fd44506093d623d35f2ad9ac5e037298a0b71
Victor Bejarano <alejandrobjrn4@gmail.com>
2024-03-23 07:05:02 -0500

    Creacion de splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,27 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "io.ionic.demo.pg.cap.ng",
   appName: "Photo Gallery Cap Ng",
   bundledWebRuntime: false,
   npmClient: "npm",
   webDir: "www",
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#0000ff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
