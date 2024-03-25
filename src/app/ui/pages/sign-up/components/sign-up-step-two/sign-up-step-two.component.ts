c7e4cbbf3a5a3574d9527a24777473b58e028d80
Victor Bejarano <alejandrobjrn4@gmail.com>
2024-03-25 00:57:08 -0500

    Completado paso 2

--- src/app/ui/pages/sign-up/components/sign-up-step-two/sign-up-step-two.component.ts
+++ src/app/ui/pages/sign-up/components/sign-up-step-two/sign-up-step-two.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
+import { IonSelect } from "@ionic/angular";
 
 @Component({
   selector: "app-sign-up-step-two",
@@ -7,6 +17,9 @@ import { FormBuilder, FormGroup } from "@angular/forms";
   styleUrls: ["./sign-up-step-two.component.scss"],
 })
 export class SignUpStepTwoComponent implements OnInit {
+  @ViewChild("typeDoc") imgElement: any;
+  @ViewChildren(IonSelect) viewChildren!: QueryList<IonSelect>;
+  @Output() next = new EventEmitter();
   form: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
@@ -16,4 +29,27 @@ export class SignUpStepTwoComponent implements OnInit {
   }
 
   ngOnInit() {}
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.viewChildren.forEach((view: any) => {
+        const shadowRoot = (view.el as HTMLElement).shadowRoot;
+        if (shadowRoot) {
+          const targetElement: any =
+            shadowRoot.querySelector(".select-wrapper");
+          if (targetElement) {
+            targetElement.style.backgroundColor = "transparent";
+            targetElement.firstChild.firstChild.style.fontFamily =
+              "AvenirLTStd-Medium";
+            targetElement.firstChild.firstChild.style.color = "#AAAAAA";
+            targetElement.firstChild.firstChild.style.fontSize = "1em";
+            console.log();
+          }
+        }
+      });
+    }, 100);
+  }
+
+  onSubmit() {
+    this.next.emit();
+  }
 }
